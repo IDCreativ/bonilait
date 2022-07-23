836f8b3f81d054004246d0c34e731f3b685086c6
IDCreativ <contact@id-creativ.com>
2022-07-23 10:52:55 +0200

    Actualités

--- web/app/themes/bc_bonilaitfood/home.php
+++ web/app/themes/bc_bonilaitfood/home.php
@@ -17,37 +17,23 @@ if (has_post_thumbnail()) {
 $page_title = isset($fields['page_header']['page_title']) && $fields['page_header']['page_title'] != "" ? $fields['page_header']['page_title'] : "<p>" . get_the_title() . "</p>";
 $page_description = isset($fields['page_header']['page_description']) && $fields['page_header']['page_description'] != "" ? $fields['page_header']['page_description'] : "";
 
+$sticky = get_option('sticky_posts');
 $actu_featured = new WP_Query(array(
 	'post_type'         => 'post',
 	'posts_per_page'    => 1,
-	'meta_query'        => array(
-		array(
-			'key'       => 'featured',
-			'compare'   => '=',
-			'value'     => true,
-		)
-	)
 ));
 $actus_not_featured = new WP_Query(array(
 	'post_type'         => 'post',
-	'posts_per_page'    => 3,
-	'meta_query'        => array(
-		array(
-			'key'       => 'featured_2',
-			'compare'   => '=',
-			'value'     => true,
-		)
-	),
-	'meta_key'  => 'order',
-	'orderby'   => 'meta_value',
-	'order'     => 'ASC'
+	'posts_per_page'    => -1,
 ));
 ?>
 <section id="post-<?php the_ID(); ?>-header" class="page-header">
 	<div class="container">
 		<div class="row">
 			<div class="col heading-container">
-				<h1><?php echo $page_title; ?></h1>
+				<h1>
+					<?php _e('Actualités', 'text-domain'); ?>
+				</h1>
 				<div class="picto-header">
 					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/h1-wave.svg'); ?>" alt="">
 				</div>
@@ -58,16 +44,8 @@ $actus_not_featured = new WP_Query(array(
 		</div>
 	</div>
 	<div class="header-overlay">
-		<svg
-			class="wave-bottom"
-			data-name="wave-bottom"
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 1600 200"
-		>
-			<path
-				class="cls-1"
-				d="M1600,8.8c-224.5,27.5-449.4,62.4-676.1,91.1C849.8,109,775.5,118.1,701,126.1,626.5,133.5,551.8,141,476.8,146s-150.1,9.2-225.5,10.4q-20.4.45-40.8.6H155.8q-65.4-.6-131.1-3.9c-8.2-.4-16.5-.9-24.7-1.4V210H1600Z"
-			/>
+		<svg class="wave-bottom" data-name="wave-bottom" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1600 200">
+			<path class="cls-1" d="M1600,8.8c-224.5,27.5-449.4,62.4-676.1,91.1C849.8,109,775.5,118.1,701,126.1,626.5,133.5,551.8,141,476.8,146s-150.1,9.2-225.5,10.4q-20.4.45-40.8.6H155.8q-65.4-.6-131.1-3.9c-8.2-.4-16.5-.9-24.7-1.4V210H1600Z" />
 		</svg>
 	</div>
 	<div class="thumb-bground">
@@ -78,15 +56,50 @@ $actus_not_featured = new WP_Query(array(
 	<?php if (have_posts()) : ?>
 		<section id="actus-grid">
 			<div class="container">
+				<div class="row row-sticky">
+					<?php
+					while ($actu_featured->have_posts()) : $actu_featured->the_post();
+						if (!is_sticky()) {
+					?>
+							<div class="actu-card-sticky">
+								<a href="<?php echo the_permalink(); ?>">
+									<div class="actu-card-single">
+										<div class="thumb-wrapper sticky-image">
+											<img src="<?php echo $featured_image; ?>" class="" alt="<?php echo $title; ?>">
+										</div>
+										<div class="infos-wrapper">
+											<div>
+												<span class="the-date"><?php echo get_the_date('F Y'); ?></span>
+												<h5 class="actu-card-title"><?php echo get_the_title(); ?></h5>
+												<p>
+													<?php echo mb_substr(get_the_excerpt(), 0, 100) . ' ...'; ?>
+												</p>
+											</div>
+											<button class="btn btn-secondary btn-sm">
+												<?php _e('En savoir plus', 'text-domain'); ?>
+											</button>
+										</div>
+									</div>
+								</a>
+							</div>
+					<?php
+						}
+					endwhile;
+
+                    wp_reset_postdata();
+					?>
+				</div>
 				<div class="row row-actu">
 					<?php
 					/* Start the Loop */
-					while (have_posts()) :
-						the_post();
+					while ($actus_not_featured->have_posts()) :
+						$actus_not_featured->the_post();
 
 						get_template_part('template-parts/content', 'category');
 
 					endwhile;
+
+                    wp_reset_postdata();
 
 					the_posts_navigation();
 
--- web/app/themes/bc_bonilaitfood/template-parts/content-category.php
+++ web/app/themes/bc_bonilaitfood/template-parts/content-category.php
@@ -8,12 +8,13 @@
  * @package bc_bonilaitfood
  */
 $title = get_the_title();
-// $title = mb_substr($title, 0, 25) . '...';
 $excerpt = get_the_excerpt();
-$excerpt = mb_substr($excerpt, 0, 180) . '...';
+$excerpt = mb_substr($excerpt, 0, 100) . ' ...';
 $featured_image = has_post_thumbnail() ? wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full')[0] : get_template_directory_uri() . "/assets/img/default/default-thumb.jpg";
+
+if (!is_sticky()) {
 ?>
-<div class="col-lg-6 col-12">
+<div class="actu-card">
 	<a href="<?php echo the_permalink(); ?>">
 		<div class="actu-card-archive">
 			<div class="thumb-wrapper">
@@ -21,13 +22,15 @@ $featured_image = has_post_thumbnail() ? wp_get_attachment_image_src(get_post_th
 			</div>
 			<div class="infos-wrapper">
 				<div>
-					<span class="the-date"><?php echo get_the_date(); ?></span>
+					<span class="the-date"><?php echo get_the_date('F Y'); ?></span>
 					<h5 class="actu-card-title"><?php echo $title; ?></h5>
+					<p>
+						<?php echo $excerpt; ?>
+					</p>
 				</div>
-				<button class="see-more small-btn">
-					<i class="fal fa-chevron-right"></i>
-				</button>
 			</div>
 		</div>
 	</a>
 </div>
+<?php
+}
